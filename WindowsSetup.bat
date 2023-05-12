56dc427bd1128b7026b29b714fb398bcce27ce44
onashia <me@onashia.com>
2023-05-11 17:11:02 -0700

    🎉 | Create WindowsSetup.bat
    WindowsSetup.bat will be used to ensure we execute with elevated permissions.

---
+++ WindowsSetup.bat
@@ -0,0 +1,30 @@
+@echo off
+
+:: Description  > Perform setup tasks for new Windows computers
+:: Updated On   > 2023-05-11
+
+
+:: Self elevate script if not run as administrator
+:-------------------------------------
+:: Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+:: If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
