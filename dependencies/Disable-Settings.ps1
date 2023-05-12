49d170e2fc6cb1900a3c39de7fe6b0789309b0b4
onashia <me@onashia.com>
2023-05-11 18:05:34 -0700

    ✨ Disable Windows welcome experience

--- dependencies/Disable-Settings.ps1
+++ dependencies/Disable-Settings.ps1
@@ -1,2 +1,27 @@
-Write-Output "We're in PowerShell now!"
-Read-Host -Prompt "Press any key to continue"
+<#
+    .SYNOPSIS
+        Disables annoying default settings in Windows 10 and 11
+    .DESCRIPTION
+        The functions in this script will disable Suggestions, Tips, and Recommendations in Windows 10 and 11    
+#>
+
+Disable-Welcome
+
+
+function Disable-Welcome {
+    <#
+        .SYNOPSIS
+            Disables the setting "Show the Windows welcome experience after updates and when signed in to show what's new and suggested"
+    #>
+
+    $RegistryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager'
+    $Name         = 'SubscribedContent-310093Enabled'
+    $Value        = '0'
+    $Type         = 'DWORD'
+
+    if (-NOT (Test-Path $RegistryPath)) {
+        New-Item -Path $RegistryPath -Force | Out-Null
+    }
+
+    New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType $Type -Force
+}
